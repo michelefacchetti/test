da919b742b5142de648c3bfe9d2c9ac9d96c824f
Michele Facchetti <michele.facchetti@gmail.com>
2014-03-06 15:14:56 +0100

    Create new classes for map feature.
    Creazione di nuove classi per la mappa

---
+++ EsempioUtilizzoGit/MapForIenerary.h
@@ -0,0 +1,13 @@
+//
+//  MapForIenerary.h
+//  EsempioUtilizzoGit
+//
+//  Created by Michele Facchetti on 06/03/14.
+//  Copyright (c) 2014 Michele Facchetti. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MapForIenerary : NSObject
+
+@end
